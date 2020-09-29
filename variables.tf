7e1aad75ae516a7387e9f510f1188dc328e60691
Rachel Sharp <rachel@hashicorp.com>
2020-09-29 18:06:40 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-variable "cidr_vpc" {
-  description = "CIDR block for the VPC"
-  default     = "10.1.0.0/16"
-}
 variable "cidr_subnet" {
   description = "CIDR block for the subnet"
   default     = "10.1.0.0/24"
@@ -10,4 +6,8 @@ variable "cidr_subnet" {
 variable "environment_tag" {
   description = "Environment tag"
   default     = "Learn"
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy your instance"
 }
